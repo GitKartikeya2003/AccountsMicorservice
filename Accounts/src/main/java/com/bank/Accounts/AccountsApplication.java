6288588ca88ba10b9e874835f2fc0b49f21f4d94
Kartikeya <kartikeyayash4@gmail.com>
2025-12-23 18:59:06 +0530

    Implemented documentation making using @OPENAPIDefinition(Swagger UI)

--- Accounts/src/main/java/com/bank/Accounts/AccountsApplication.java
+++ Accounts/src/main/java/com/bank/Accounts/AccountsApplication.java
@@ -1,15 +1,40 @@
 package com.bank.Accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts microservice Rest api documentation",
+                description = "EasyBank Accounts microservice REST Api documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Kartikeya Gupta",
+                        email = "kartikeyayash4@gmail.com",
+                        url = "http://aogrinoanorbn"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "http://LicenseLink"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "aoignworinwir",
+                url = "http://ExternalDocsLink"
+        )
+)
 public class AccountsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsApplication.class, args);
+    }
 
 }
